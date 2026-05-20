f31bcae3c22031b03538db72fe6208d78b08c1db
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2026-05-20 18:31:53 +0300

    Mongo DB connection
    -Added MongoDB to .env file;
    -Added Mongoose and connection to -MongoDB;

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { connectMongoDB } from './db/connectMomgoDB.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3030;
+
+//Глобальні Middleware
+app.use(cors()); // дозвіл для запитів з інших доменів
 
 // Перший маршрут
 app.get('/api/dashboard', (req, res) => {
   res.status(200).json({ message: 'Hello world!' });
 });
+
+//підключення до MongoDB
+await connectMongoDB();
 
 // Запуск сервера
 app.listen(PORT, () => {
